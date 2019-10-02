f295c3de06f74b4a627a4eaa86176cec3e1864f4
Thibault Debatty <thibault.debatty@gmail.com>
2019-10-02 11:43:12 +0200

    Updated dependencies to laravel 6

--- test.sh
+++ test.sh
@@ -4,23 +4,13 @@ cd /tmp
 git clone git@github.com:tdebatty/laravel-resource-generator-test.git
 cd laravel-resource-generator-test/laravel-5.6
 composer install
+composer require tdebatty/laravel-resource-generator:dev-master
 cp .env.example .env
 touch storage/app/db.sqlite
 php artisan migrate
+php artisan key:generate
 
 php artisan resource-generator:generate Product
 php artisan migrate
 ./vendor/bin/phpunit
-RESULT=$?
 
-
-echo "Cleanup..."
-rm storage/app/db.sqlite
-rm app/Product.php
-rm app/Http/Controllers/ProductController.php
-rm database/migrations/*_create_products_table.php
-rm -Rf resources/views/product/
-
-sed -i "/Route::resource('app\/products', 'ProductController');/d" routes/web.php
-
-exit $RESULT
